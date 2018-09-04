44306956495435dc64208a497a1e8776229da818
28877110 <441423487@qq.com>
2018-09-04 13:34:58 +0800

    init

---
+++ src/main/java/GoogleGuavaUtil.java
@@ -0,0 +1,8 @@
+/**
+ * @author smallfish
+ * @Title: GoogleGuavaUtil
+ * @ProjectName toolMain
+ * @date 18-9-4下午1:34
+ */
+public class GoogleGuavaUtil {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author smallfish
+ * @Title: Main
+ * @ProjectName toolMain
+ * @date 18-9-3下午2:51
+ */
+public class Main {
+    public static void main(String[] arg){
+
+        System.out.println("hel");
+    }
+}
