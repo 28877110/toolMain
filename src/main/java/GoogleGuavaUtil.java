cb3f6abef86f29e61e3fd644441bc26bbe6771c5
28877110 <441423487@qq.com>
2018-09-09 14:23:34 +0800

    add google guara

--- src/main/java/GoogleGuavaUtil.java
+++ src/main/java/GoogleGuavaUtil.java
@@ -1,3 +1,14 @@
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author smallfish
  * @Title: GoogleGuavaUtil
@@ -6,4 +17,47 @@
  */
 
 public class GoogleGuavaUtil {
+    //google Guava的一些功能汇总整理一下
+
+    public void ImmutableMultimapUtil(){
+        //不可变map
+        ImmutableMultimap<String,String> immutableMultimap1 = ImmutableMultimap.of("name","Tom","age","12");
+        System.out.println(immutableMultimap1);
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("name","Jack");
+        hashMap.put("age","13");
+        ImmutableMultimap<String,String> immutableMultimap2 = ImmutableMultimap.copyOf(immutableMultimap1);
+        System.out.println(immutableMultimap2);
+    }
+
+    //主要字符串分割和对list,map的拼合
+    public void strUtil(){
+        Joiner joiner = Joiner.on(";").skipNulls();
+        //Harry;;Ron;Hermione
+        System.out.println(joiner.join("Harry","", "Ron", "Hermione"));
+        //Harry;Ron;Hermione
+        System.out.println(joiner.join("Harry",null, "Ron", "Hermione"));
+        ArrayList<String> arrayList = Lists.newArrayList("John", "Jane", null, "Tom");
+        //John,Jane,Tom       (跳过null)
+        System.out.println(Joiner.on(",").skipNulls().join(arrayList));
+        //John,Jane,hello,Tom    (替换null)
+        System.out.println(Joiner.on(",").useForNull("hello").join(arrayList));
+
+        HashMap<String,String> hashMap = Maps.newHashMap();
+        hashMap.put("name","Jack");
+        hashMap.put("age","13");
+        hashMap.put("grade","");
+        //grade=&&name=Jack&&age=13
+        System.out.println(Joiner.on("&&").withKeyValueSeparator("=").join(hashMap));
+
+        String input = "grade=&&name=Jack&&age=13";
+        Map<String, String> result = Splitter.on("&&")
+                .withKeyValueSeparator("=")
+                .split(input);
+        //{grade=, name=Jack, age=13}
+        System.out.println(result);
+
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,8 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public String orderlyQueue(String S, int K) {
-        if (K > 1) {
-            char S2[] = S.toCharArray();
-            Arrays.sort(S2);
-            return new String(S2);
-        }
-        String res = S;
-        for (int i = 1; i < S.length(); i++) {
-            String tmp = S.substring(i) + S.substring(0, i);
-            if (res.compareTo(tmp) > 0) res = tmp;
-        }
-        return res;
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println(main.orderlyQueue("baaca",3));
+        GoogleGuavaUtil googleGuavaUtil = new GoogleGuavaUtil();
+        googleGuavaUtil.strUtil();
     }
 }
