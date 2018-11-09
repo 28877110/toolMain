e80150006262c6a062fe6758b1e0071aa8b260d2
28877110 <441423487@qq.com>
2018-11-09 19:54:08 +0800

    add nio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import nio.ChannelBasic;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
--- src/main/java/nio/ChannelBasic.java
+++ src/main/java/nio/ChannelBasic.java
@@ -2,8 +2,6 @@ package nio;
 
 import lombok.Cleanup;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -12,19 +10,20 @@ import java.nio.channels.FileChannel;
 public class ChannelBasic {
     public void fileChannelToBuffer() throws IOException {
         //创建这个文件的目的是为了getChannel()  getChannel()是为了得到
-        RandomAccessFile randomAccessFile = new RandomAccessFile("./test.txt","rw");
-        @Cleanup  FileChannel fileChannel = randomAccessFile.getChannel();
+        RandomAccessFile randomAccessFile = new RandomAccessFile("./test.txt", "rw");
+        @Cleanup FileChannel fileChannel = randomAccessFile.getChannel();
 
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer buffer = ByteBuffer.allocate(10);
         int index = fileChannel.read(buffer);
-        while(index!=-1){
-            System.out.println("读取内容"+index);
+        while (index != -1) {
+            System.out.println("读取内容" + index);
             buffer.flip();
-            while(buffer.hasRemaining()){
+            while (buffer.hasRemaining()) {
                 System.out.println(buffer.get());
             }
             buffer.clear();
             index = fileChannel.read(buffer);
         }
+
     }
 }
