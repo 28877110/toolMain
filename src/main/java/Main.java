459d7a1e1420136b79f79e879f115d4c63e7d9e7
28877110 <441423487@qq.com>
2018-11-11 22:30:41 +0800

    add zjgsu login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import nio.ChannelBasic;
-
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("hello world");
-        ChannelBasic channelBasic = new ChannelBasic();
-        channelBasic.fileChannelToBuffer();
     }
 
 }
