f18026c8cbd870da8c2b702dab5fd66a57678b23
28877110 <441423487@qq.com>
2018-11-29 18:00:20 +0800

    add Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        System.out.println("hello world");
-//        ChannelBasic channelBasic = new ChannelBasic();
-//        channelBasic.fileChannelToBuffer();
-        String a = "asdaws啊山东阿萨德";
-        System.out.println(stringFilter(a));
-
-    }
-
-    private static String stringFilter(String str) {
-        return str.replaceAll("[^\u4E00-\u9FA5\u3000-\u303F\uFF00-\uFFEF\u0000-\u007F\u201c-\u201d]", " ");
+        System.out.println("hello world");
     }
 
 }
