4a144db555d6fe534d53fcad8cd58fc45cf0b3bb
28877110 <441423487@qq.com>
2018-12-03 20:53:11 +0800

    :bulb: :alembic: :beers:

--- src/main/java/bean/Person.java
+++ src/main/java/bean/Person.java
@@ -1,14 +1,14 @@
 package bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String Id;
     private String name;
 
-    public Person(String Id, String name) {
-        this.Id = Id;
-        this.name = name;
-    }
 }
