420361a54bd6d0509f653d8782953e21f727c322
28877110 <441423487@qq.com>
2018-12-24 19:15:47 +0800

    :bulb: :alembic: :beers:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import JUC.AtomicBase;
 import JUC.PublishAndEscape;
 import Other.SystemTime;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        SystemTime systemTime = new SystemTime();
-        systemTime.SystemTime1();
+        AtomicBase atomicBase = new AtomicBase();
+        atomicBase.AtomicTest();
         System.out.println("hello world");
     }
 
