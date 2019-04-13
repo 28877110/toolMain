a6c3fafa97f60643a4a61bb441d231ce75b689b2
28877110 <441423487@qq.com>
2019-04-13 21:30:06 +0800

    :bulb: :alembic: :beers:

---
+++ src/main/java/Other/PermuteProblem.java
@@ -0,0 +1,43 @@
+package Other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <Description> 全排列问题的解决办法<br>
+ * 具体leetcode的46题
+ *
+ * @author smallfish<br>
+ * @version 1.0<br>
+ * @createDate 2018/12/24 22:36 <br>
+ * @see Other <br>
+ */
+public class PermuteProblem {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        help(nums,0,result);
+        return result;
+    }
+
+    public void help(int nums[],int index ,List<List<Integer>> result){
+        if(index == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for(int i : nums) {
+                temp.add(i);
+            }
+            result.add(temp);
+            return;
+        }
+        for(int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            help(nums, index + 1, result);
+            swap(nums, index, i);
+        }
+    }
+
+    public void swap(int num[],int a,int b ){
+        int tem =num[a] ;
+        num[a] = num[b];
+        num[b] = tem;
+    }
+}
--- src/main/java/bean/Person.java
+++ src/main/java/bean/Person.java
@@ -11,5 +11,4 @@ public class Person {
     private String Id;
     private String name;
     private int success;
-
 }
